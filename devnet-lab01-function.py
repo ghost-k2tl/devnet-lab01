0622c78030f59219f2d7682aac8f83a0f343eb0f
ghost-k2tl <ghost.k2tl@gmail.com>
2020-09-16 11:44:51 +0700

    update

--- devnet-lab01-function.py
+++ devnet-lab01-function.py
@@ -21,9 +21,15 @@ cong2so(100,200)
 
 
 def chia2so(x,y):
-    thuong = x / y
-    print('So bi chia:{0}'.format(x))
-    print('So chia:{0}'.format(y))
-    print("Thuong 2 so la " + str(thuong))
+    if type(x) == int and type(y) == int:
+        if y == 0:
+            print("Khong duoc chia cho 0")
+        else :
+            thuong = x / y
+            print('So bi chia:{0}'.format(x))
+            print('So chia:{0}'.format(y))
+            print("Thuong 2 so la " + str(thuong))
+    else :
+        print("Gia tri dau vao khong hop le")
 
-chia2so(99,3)
+chia2so("name",0)
