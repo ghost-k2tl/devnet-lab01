db10f9ba61933d1bcceae9ecd21585a9e610b79d
ghost-k2tl <ghost.k2tl@gmail.com>
2020-09-16 11:27:52 +0700

    update

---
+++ devnet-lab01-conditon.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 11:09:39 2020
+
+@author: admin
+"""
+
+
+x = 9
+if x > 15:
+    print("Lon hon 15")
+elif x > 10:
+    print("Lon hon 10")
+else:
+    print("Nho hon 10")
---
+++ devnet-lab01-function.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 11:13:57 2020
+
+@author: admin
+"""
+
+
+def hienthi_ten():
+    print("Ten toi la Phong")
+    
+hienthi_ten()
+
+def cong2so(x,y):
+    tong = x + y
+    print('X:{0}'.format(x))
+    print('Y:{0}'.format(y))
+    print("Tong 2 so "+ str(x) + " va " + str(y) + " la " + str(tong))
+    
+cong2so(100,200)
+
+
+def chia2so(x,y):
+    thuong = x / y
+    print('So bi chia:{0}'.format(x))
+    print('So chia:{0}'.format(y))
+    print("Thuong 2 so la " + str(thuong))
+
+chia2so(99,3)
